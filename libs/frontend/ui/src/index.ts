59b00e8f06f86934846d4836976f76ae2ca93321
Sietsethooft <147145688+Sietsethooft@users.noreply.github.com>
2024-12-19 10:30:10 +0100

    header component creation
    - Removed ui component
    + added header compontent

--- libs/frontend/ui/src/index.ts
+++ libs/frontend/ui/src/index.ts
@@ -1,4 +1,4 @@
-export * from './lib/ui/ui.component';
+export * from './lib/ui/header/header.component';
 export * from './lib/welcome/welcome.component';
 export * from './lib/dashboard/dashboard.component';
 export * from './lib/ui.module';
