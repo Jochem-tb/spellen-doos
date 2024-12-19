ca4340dda9445a184c4ceeaeebe005a1ac707d24
Clemens <100091164+Clemens00505@users.noreply.github.com>
2024-12-19 09:37:09 +0100

    Feature/welcome (#1)
    * Welcome and Routing
    
    Added:
     Welcome component
     Routing to Welcome
    
    * Updated .gitignore
    
    * Changes in Buttons and background

---
+++ apps/spellen-doos/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { appRoutes } from './app.routes';
+import { UiModule } from '@spellen-doos/ui';
+
+
+
+@NgModule({
+    declarations: [
+        AppComponent
+      ],
+      imports: [
+        BrowserModule,
+        RouterModule.forRoot(appRoutes),
+        UiModule
+      ],
+      providers: [],
+      bootstrap: [AppComponent]
+})
+export class AppModule {}
