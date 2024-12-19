6b020d2eca8772a865ff2b629d1e7aa2f90e3800
Clemens <100091164+Clemens00505@users.noreply.github.com>
2024-12-19 09:40:42 +0100

    Revert "Feature/welcome (#1)"
    This reverts commit ca4340dda9445a184c4ceeaeebe005a1ac707d24.

--- apps/spellen-doos/src/app/app.routes.ts
+++ apps/spellen-doos/src/app/app.routes.ts
@@ -1,10 +1,3 @@
 import { Route } from '@angular/router';
-import { WelcomeComponent } from '@spellen-doos/ui';
 
-export const appRoutes: Route[] = [
-
-    { path: '', redirectTo: 'welcome', pathMatch: 'full' },
-    { path: 'welcome', component: WelcomeComponent },
-
-    { path: '**', redirectTo: 'welcome' }
-];
+export const appRoutes: Route[] = [];
--- libs/frontend/ui/src/index.ts
+++ libs/frontend/ui/src/index.ts
@@ -1,3 +1,1 @@
 export * from './lib/ui/ui.component';
-export * from './lib/welcome/welcome.component';
-export * from './lib/ui.module';
