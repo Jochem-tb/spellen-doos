86fd004e608fe7a0ea49b6a1cf7ae700e0baedfa
Clemens <100091164+Clemens00505@users.noreply.github.com>
2024-12-19 09:42:45 +0100

    Feature/welcome (#2)
    * Welcome and Routing
    
    Added:
     Welcome component
     Routing to Welcome
    
    * Updated .gitignore
    
    * Changes in Buttons and background
    
    * Revert "Updated .gitignore"
    
    This reverts commit 65d32077998624c10629e12ed2cc8e14f76c0a98.

--- apps/spellen-doos/src/app/app.routes.ts
+++ apps/spellen-doos/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Route } from '@angular/router';
+import { WelcomeComponent } from '@spellen-doos/ui';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+
+    { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+    { path: 'welcome', component: WelcomeComponent },
+
+    { path: '**', redirectTo: 'welcome' }
+];
