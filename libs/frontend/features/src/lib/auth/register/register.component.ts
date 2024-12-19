54d627acde788d61f9059238a32876e0786aeb22
Clemens <100091164+Clemens00505@users.noreply.github.com>
2024-12-19 14:42:18 +0100

    Register and Login
    Margins
    Validation verbeterd
    Standard value for dateOfBirth

--- libs/frontend/features/src/lib/auth/register/register.component.ts
+++ libs/frontend/features/src/lib/auth/register/register.component.ts
@@ -1,6 +1,5 @@
-// register.component.ts
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ValidatorFn, AbstractControl } from '@angular/forms';
 import { Router } from '@angular/router';
 import { REGISTER_STEPS } from '@spellen-doos/shared/api';
 
@@ -10,18 +9,58 @@ import { REGISTER_STEPS } from '@spellen-doos/shared/api';
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css'],
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
   steps = REGISTER_STEPS;
   currentStepIndex = 0;
   registerForm: FormGroup;
+  dateToday: string | undefined;
 
   constructor(private fb: FormBuilder, private router: Router) {
-    this.registerForm = this.fb.group({
-      name: ['', Validators.required],
-      dateOfBirth: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(8)]],
-    });
+    this.registerForm = this.fb.group(
+      {
+        name: ['', Validators.required],
+        // user needs to be 65 years old to register so we set the min age to 65 but check from date of birth
+        dateOfBirth: ['', [Validators.required, this.minAgeValidator(65)]],
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', [Validators.required, Validators.minLength(8)]],
+        confirmPassword: ['', Validators.required],
+      },
+      { validators: this.passwordMatchValidator() }
+    );
+  }
+
+  ngOnInit() {
+    // Get current date in YYYY-MM-DD format
+    const currentDate = new Date();
+    this.dateToday = currentDate.toISOString().split('T')[0]; // Format the date to YYYY-MM-DD
+    this.registerForm.patchValue({ dateOfBirth: this.dateToday });
+  }
+
+  minAgeValidator(minAge: number): ValidatorFn {
+    return (control: AbstractControl) => {
+      const dateOfBirth = new Date(control.value);
+      const currentDate = new Date();
+      const age = currentDate.getFullYear() - dateOfBirth.getFullYear();
+      if (age < minAge) {
+        return { minAge: true };
+      }
+      return null;
+    };
+  }
+
+  passwordMatchValidator(): ValidatorFn {
+    return (control: AbstractControl) => {
+      const formGroup = control as FormGroup;
+      const password = formGroup.get('password')?.value;
+      const confirmPassword = formGroup.get('confirmPassword')?.value;
+
+      if (password !== confirmPassword) {
+        formGroup.get('confirmPassword')?.setErrors({ mismatch: true });
+      } else {
+        formGroup.get('confirmPassword')?.setErrors(null);
+      }
+      return null;
+    };
   }
 
   get currentStep() {
@@ -43,7 +82,7 @@ export class RegisterComponent {
   onSubmit() {
     if (this.registerForm.valid) {
       console.log(this.registerForm.value);
-      // Verwerk de registratie
+      // Process registration
       this.router.navigate(['/dashboard']);
     }
   }
