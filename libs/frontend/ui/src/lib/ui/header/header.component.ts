a832653eb3980d1c91799ea1db26556a6dd82335
Jochem-tb <88931096+Jochem-tb@users.noreply.github.com>
2024-12-20 12:01:18 +0100

    Dynamic loading name+picuter in header

--- libs/frontend/ui/src/lib/ui/header/header.component.ts
+++ libs/frontend/ui/src/lib/ui/header/header.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProfileService } from '../../profile/profile.service';
+import { IUser } from '@spellen-doos/shared/api';
 
 @Component({
   selector: 'app-header',
@@ -6,4 +8,13 @@ import { Component } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
-export class HeaderComponent {}
+export class HeaderComponent implements OnInit {
+  public profile: IUser | null = null;
+  constructor(private profileService: ProfileService) {}
+
+  ngOnInit(): void {
+    this.profileService.getProfile().subscribe((profile) => {
+      this.profile = profile;
+    });
+  }
+}
