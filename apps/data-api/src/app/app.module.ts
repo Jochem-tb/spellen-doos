a1b82e039e4e448ddf979dbaf80089541923cc73
Clemens <100091164+Clemens00505@users.noreply.github.com>
2025-01-06 14:25:47 +0100

    Added connection

--- apps/data-api/src/app/app.module.ts
+++ apps/data-api/src/app/app.module.ts
@@ -1,9 +1,24 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from '@spellen-doos/auth';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_URI, {
+      connectionFactory: (connection) => {
+        connection.on('connected', () => {
+          Logger.verbose('Connected to MongoDB', `${process.env.MONGO_URI}`);
+        });
+        connection._events.connected();
+        return connection;
+      },
+    }),
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
