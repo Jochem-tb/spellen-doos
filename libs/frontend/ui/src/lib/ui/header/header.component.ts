2493f38ca6987e869bf40c3e46dd7456a520ca0d
Clemens <100091164+Clemens00505@users.noreply.github.com>
2025-01-16 12:44:44 +0100

    Logout button only shows on dashboard

--- libs/frontend/ui/src/lib/ui/header/header.component.ts
+++ libs/frontend/ui/src/lib/ui/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router'; // Import Router
 import { ProfileService } from '../../profile/profile.service';
 import { IUser } from '@spellen-doos/shared/api';
 import { AuthService } from '@spellen-doos/features';
@@ -12,11 +13,18 @@ import { AuthService } from '@spellen-doos/features';
 export class HeaderComponent implements OnInit {
   public profile: IUser | null = null;
   private userId: string | null = null;
-  constructor(private profileService: ProfileService, private authService: AuthService) {}
+  logoutShown = false;
+
+  constructor(
+    private profileService: ProfileService,
+    private authService: AuthService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.userId = localStorage.getItem('userId');
     console.log('Retrieved userId from localStorage:', this.userId);
+
     if (this.userId) {
       this.profileService.getProfileById(this.userId).subscribe((profile) => {
         this.profile = profile;
@@ -24,6 +32,12 @@ export class HeaderComponent implements OnInit {
     } else {
       console.error('User ID is null');
     }
+
+    this.router.events.subscribe(() => {
+      const currentRoute = this.router.url.split('/').pop();
+      console.log('[DEBUG] Current route:', currentRoute);
+      this.logoutShown = currentRoute === 'dashboard';
+    });
   }
 
   logout(): void {
