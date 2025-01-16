9e98506c2d135fbb3cff81c9316a038df8504d45
Sietsethooft <147145688+Sietsethooft@users.noreply.github.com>
2025-01-16 12:51:13 +0100

    Creation component

--- libs/frontend/games/src/lib/gameTutorial/gameTutorial.component.ts
+++ libs/frontend/games/src/lib/gameTutorial/gameTutorial.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { GameTutorialService } from './gameTutorial.service';
 
 @Component({
   selector: 'lib-game-tutorial',
-  imports: [CommonModule],
+  standalone: false,
   templateUrl: './gameTutorial.component.html',
   styleUrl: './gameTutorial.component.css',
 })
-export class GameTutorialComponent {}
+export class GameTutorialComponent implements OnInit{
+  tutorialContent = 'Aan het laden...'; // Default content while loading
+  gameTitle = '' // Default content while loading
+
+  constructor(
+    private router: Router,
+    private gameTutorialService: GameTutorialService
+  ) {}
+
+  ngOnInit(): void {
+    // Listen for route changes
+        // this.router.events.subscribe((event) => {
+        //   if (event instanceof NavigationEnd) {
+        //     this.loadTutorial(this.router.url);
+        //   }
+        // });
+        this.loadTutorialMock();
+  }
+
+  loadTutorial(url: string) {
+    this.tutorialContent = '<h5>Aan het laden...</h5>'; // Set loading state
+    this.gameTutorialService.getHelpContent(url).subscribe({
+      next: (results) => (
+        (this.tutorialContent = results.content), (this.gameTitle = results.title)
+      ),
+      error: (err) => {
+        console.error('Failed to load help content', err);
+        this.tutorialContent =
+          '<h3>Oeps.</h3><h5>Er is iets fout gegaan. Probeer het later nog een keer.</h5>';
+        this.gameTitle = '';
+      },
+    });
+  }
+
+  loadTutorialMock() {
+    console.log('Loading mock tutorial content');
+    this.gameTutorialService.getHelpContentMock('mock').subscribe({
+      next: (results) => {
+        this.tutorialContent = results;
+        this.gameTitle = 'Mock Title'; // Voeg een mock titel toe als je dat wilt
+      },
+      error: (err) => console.error('Error loading tutorial content', err),
+      complete: () => console.log('Tutorial content loaded')
+    });
+  }
+}
