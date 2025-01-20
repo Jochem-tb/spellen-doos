863893259b915c2892f4e3c0fb590a5e0c2784e8
Jochem-tb <88931096+Jochem-tb@users.noreply.github.com>
2025-01-21 00:26:39 +0100

    QuickFix home network testing

--- libs/shared/util-env/src/lib/environment.ts
+++ libs/shared/util-env/src/lib/environment.ts
@@ -7,11 +7,11 @@ export const environment: IEnvironment = {
 
   //* API for CRUD operations
   // dataApiUrl: 'http://localhost:3000/api',
-  dataApiUrl: 'http://192.168.178.204:3000/api',
-  // dataApiUrl:  'https://spellen-doos-api.azurewebsites.net/api',
+  // dataApiUrl: 'http://192.168.178.204:3000/api',
+  dataApiUrl: 'https://spellen-doos-api.azurewebsites.net/api',
 
   //* Socket URL for games
-  // socketUrl: 'http://localhost:3000',
-  socketUrl: 'http://192.168.178.204:3000',
+  socketUrl: 'http://localhost:3000',
+  // socketUrl: 'http://192.168.178.204:3000',
   //  socketUrl: 'https://spellen-doos-api.azurewebsites.net'
 };
