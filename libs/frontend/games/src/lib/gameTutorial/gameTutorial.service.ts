c8dd03e8a8ba1c2f1b8222f528c380928345a8f4
Sietsethooft <147145688+Sietsethooft@users.noreply.github.com>
2025-01-20 11:02:20 +0100

    Merge fix

--- libs/frontend/games/src/lib/gameTutorial/gameTutorial.service.ts
+++ libs/frontend/games/src/lib/gameTutorial/gameTutorial.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map, Observable } from 'rxjs';
-import { IGame, IHelpButton } from '@spellen-doos/shared/api';
+import { IGame} from '@spellen-doos/shared/api';
+import { environment } from '@spellen-doos/environment';
 
 @Injectable({
   providedIn: 'root',
@@ -11,7 +12,7 @@ export class GameTutorialService {
     
   getGameContent(gameId: string) : Observable<IGame> {
     return this.http
-      .get<{ results: IGame }>('http://localhost:3000/api/game/' + gameId)
+      .get<{ results: IGame }>(`${environment.dataApiUrl}/game/${gameId}`)
       .pipe(map((response) => response.results));
   }
 }
