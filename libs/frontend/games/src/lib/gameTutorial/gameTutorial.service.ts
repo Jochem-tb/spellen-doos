f7c7d4e41d4c1be172e37b843651f1fae23bb90b
Sietsethooft <147145688+Sietsethooft@users.noreply.github.com>
2025-01-20 10:46:17 +0100

    Onnodige functies verwijderd

--- libs/frontend/games/src/lib/gameTutorial/gameTutorial.service.ts
+++ libs/frontend/games/src/lib/gameTutorial/gameTutorial.service.ts
@@ -14,24 +14,4 @@ export class GameTutorialService {
       .get<{ results: IGame }>('http://localhost:3000/api/game/' + gameId)
       .pipe(map((response) => response.results));
   }
-
-  getHelpContentMock(route: string): Observable<string> {
-    console.log('mock aangeroepen');
-    return new Observable<string>((observer) => {
-      observer.next(`
-                Steen, Papier, Schaar is een klassiek spel dat gespeeld wordt met twee spelers. Beide spelers kiezen tegelijkertijd een van de drie opties: steen, papier of schaar. 
-                <br><br>
-                De regels gaan als volgt:
-                <br>
-                - Steen verslaat schaar (steen breekt schaar).
-                <br>
-                - Schaar verslaat papier (schaar knipt papier).
-                <br>
-                - Papier verslaat steen (papier bedekt steen).
-                <br><br>
-                Het doel is om je tegenstander te verslaan door een optie te kiezen die hun keuze verslaat.
-        `);
-      observer.complete();
-    });
-  }
 }
